18cb2993e331cfb2d8f93abe51fd0a5d7fbd1912
Angelo Deleon <rudy209@comcast.net>
2019-08-26 10:28:06 -0700

    added login and register components and setup imports

--- lambda-mud-frontend/src/App.js
+++ lambda-mud-frontend/src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App-container">
+        <Route exact path='/' component={Login} />
+        <Route path='/register' component={Register} />
+      </div>
+    );
+  }
 }
 
 export default App;
