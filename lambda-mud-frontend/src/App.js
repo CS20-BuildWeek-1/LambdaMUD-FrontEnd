c52d983e72ef84412803584d5459e17ed7d462b0
Austin James <apj2277@gmail.com>
2019-08-26 14:32:44 -0700

    adventure pulling test data

--- lambda-mud-frontend/src/App.js
+++ lambda-mud-frontend/src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
+import React from "react";
+import { Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Adventure from "./components/Adventure";
 
-import './App.css';
+import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App-container">
-        <Route exact path='/' component={Login} />
-        <Route path='/register' component={Register} />
+        <Route exact path="/" component={Login} />
+        {/* <Route path="/register" component={Register} /> */}
+        <Route path="/adventure" component={Adventure} />
       </div>
     );
   }
